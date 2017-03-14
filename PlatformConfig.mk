65e699891c97910116cffcf042261bac94f6f626
Greg Willard (r3pwn) <wickett06@gmail.com>
2017-03-14 05:16:26 -0400

    HACK: set selinux permissive until the device tree is finalized

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -28,6 +28,8 @@ TARGET_CPU_VARIANT := krait
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_CMDLINE := panic=3 console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+# this is only temporary, until the device tree is finalized
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 # Kernel properties
