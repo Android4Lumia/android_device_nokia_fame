55cf8bd8bab3ed4bb9efaff3a10952762f449304
Greg Willard (r3pwn) <wickett06@gmail.com>
2017-06-21 03:57:13 -0400

    BoardConfig: split up the cmdline it makes it look neater, plus it wasn't letting me build without this

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,8 +41,14 @@ TARGET_KERNEL_SOURCE := kernel/nokia/msm8x27
 TARGET_KERNEL_CONFIG := cyanogenmod_fame_defconfig
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_CMDLINE := panic=3 console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
-ifeq($(TARGET_BUILD_VARIANT),eng)
+BOARD_KERNEL_CMDLINE := panic=3 \
+    console=ttyHSL0,115200,n8 \
+    androidboot.hardware=qcom \
+    user_debug=23 \
+    msm_rtb.filter=0x3F \
+    ehci-hcd.park=3 \
+    androidboot.bootdevice=msm_sdcc.1
+ifeq ($(TARGET_BUILD_VARIANT),eng)
 # set selinux permissive if it's an eng build
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 endif
